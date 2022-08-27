4fa65302c93064b6592dc97d548c8da7f2381fdd
Joao Souza <joaonetotfm@gmail.com>
2022-08-27 16:41:50 -0300

    feature: implements nav-bar

--- src/app/modules/shared-module/shared-module.module.ts
+++ src/app/modules/shared-module/shared-module.module.ts
@@ -1,12 +1,15 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModuleModule } from './../material-module/material-module.module';
 import { NgModule } from '@angular/core';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   imports: [MaterialModuleModule, ReactiveFormsModule],
-  declarations: [],
-  exports: [MaterialModuleModule, ReactiveFormsModule],
+  declarations: [
+    NavBarComponent
+  ],
+  exports: [MaterialModuleModule, ReactiveFormsModule, NavBarComponent],
 
 })
 export class SharedModuleModule { }
