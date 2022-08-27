7502d1aa5c556b0edafc82bebf8fd2e4f16fbe6e
Joao Souza <joaonetotfm@gmail.com>
2022-08-27 20:55:49 -0300

    wip: algoritm rsa

--- src/app/modules/encript-decript/encript-decript.component.ts
+++ src/app/modules/encript-decript/encript-decript.component.ts
@@ -12,12 +12,63 @@ export class EncriptDecriptComponent implements OnInit {
     this.form = new FormGroup({
       enteredData: new FormControl(null, [Validators.required]),
       enteredDataEncripted: new FormControl(null),
-      primaryKey: new FormControl(null),
-      secondatyKey: new FormControl(null),
-    })
+      publicKeys: new FormControl([]),
+      privateKeys: new FormControl([]),
+    });
   }
 
   ngOnInit() {
     this.form.get('enteredDataEncripted')?.disable();
+  }
+  encript(): void {
+    const enteredData = this.form.get('enteredData')?.value;
+    let enteredDataInASCII = [];
+    for (let i = 0; i < enteredData.length; i++) {
+      enteredDataInASCII.push(enteredData.charCodeAt(i));
+    }
+    console.log(enteredDataInASCII)
+  }
+  generateKeys(): void {
+    let p;
+    let q;
+    while (!p) {
+      p = this.generatePrime();
+    }
+    while (!q) {
+      q = this.generatePrime();
+    }
+    const n = p * q;
+    this.form.get('publicKeys')?.value.push(n);
+    this.generateE(this.totient(p, q));
+  }
+
+  totient(p: number, q: number): number {
+    const phiN = (p - 1) * (q - 1);
+    return phiN;
+  }
+  generateE(totientValue: number): any {
+    const possibleE = Math.floor(Math.random() * (totientValue - 1000) + 1000);
+    if (possibleE > 1 && possibleE < totientValue) {
+      this.form.get('publicKeys')?.value.push(possibleE);
+      return possibleE;
+    } else {
+      this.generateE(totientValue);
+    }
+  }
+
+  generatePrime(): any {
+    const number = Math.floor(Math.random() * 1000);
+    let dividers: number[] = [];
+    for (let i = 1; i <= number; i++) {
+      const rest = number % i;
+      if (!rest) {
+        dividers.push(i);
+      }
+    }
+    if (dividers.length === 2 && dividers[0] === 1 && dividers[1] === number) {
+      return dividers[1];
+    } else {
+      this.generatePrime();
+    }
   }
 }
