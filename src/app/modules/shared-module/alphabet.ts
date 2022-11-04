842150d2c7d80980bf11b7792d2c0e07d5c77a19
Joao Souza <joaonetotfm@gmail.com>
2022-11-04 16:01:16 -0300

    feature: transform alphabet

---
+++ src/app/modules/shared-module/alphabet.ts
@@ -0,0 +1,114 @@
+export class Alphabet {
+  static list() {
+    return [
+      {
+        letter: 'a',
+        value: 2,
+      },
+      {
+        letter: 'b',
+        value: 3,
+      },
+      {
+        letter: 'c',
+        value: 4,
+      },
+      {
+        letter: 'd',
+        value: 5,
+      },
+      {
+        letter: 'e',
+        value: 6,
+      },
+      {
+        letter: 'f',
+        value: 7,
+      },
+      {
+        letter: 'g',
+        value: 8,
+      },
+      {
+        letter: 'h',
+        value: 9,
+      },
+      {
+        letter: 'i',
+        value: 10,
+      },
+      {
+        letter: 'j',
+        value: 11,
+      },
+      {
+        letter: 'k',
+        value: 12,
+      },
+      {
+        letter: 'l',
+        value: 13,
+      },
+      {
+        letter: 'm',
+        value: 14,
+      },
+      {
+        letter: 'n',
+        value: 15,
+      },
+      {
+        letter: 'o',
+        value: 16,
+      },
+      {
+        letter: 'p',
+        value: 17,
+      },
+      {
+        letter: 'q',
+        value: 18,
+      },
+      {
+        letter: 'r',
+        value: 19,
+      },
+      {
+        letter: 's',
+        value: 20,
+      },
+      {
+        letter: 't',
+        value: 21,
+      },
+      {
+        letter: 'u',
+        value: 22,
+      },
+      {
+        letter: 'v',
+        value: 23,
+      },
+      {
+        letter: 'w',
+        value: 24,
+      },
+      {
+        letter: 'x',
+        value: 25,
+      },
+      {
+        letter: 'y',
+        value: 26,
+      },
+      {
+        letter: 'z',
+        value: 27,
+      },
+      {
+        letter: ' ',
+        value: 28,
+      },
+    ];
+  }
+}
