dc8155277d9952948bb7d3498af6b97a315a1b8b
joao <joaonetotfm@gmail.com>
2022-11-27 12:50:27 -0300

    fix: erros

--- src/app/modules/encript-decript/decript/decript.component.ts
+++ src/app/modules/encript-decript/decript/decript.component.ts
@@ -1,6 +1,7 @@
 import { EncriptService } from './../services/encript.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Alphabet } from '../../shared-module/alphabet';
 
 @Component({
   selector: 'app-decript',
@@ -9,7 +10,9 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class DecriptComponent implements OnInit {
   form!: FormGroup;
+  alphabet = Alphabet.list();
   dencriptedData!: number[];
+  dencriptedDataString!: string[];
   constructor(private decriptService: EncriptService) {
     this.form = new FormGroup({
       p: new FormControl(null, Validators.required),
@@ -19,20 +22,33 @@ export class DecriptComponent implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
   dencript(): void {
     let d: number = 2;
     const { p, q, e, enteredData } = this.form.value;
-    const splitedEnteredData = enteredData.split(',');
+    const splitedEnteredData = enteredData.split(',' || ' ');
     this.decriptService
       .desencript({ p, q, e, message: splitedEnteredData })
-      .subscribe((res) => {
-        this.dencriptedData = res.dencripted_message;
+      .subscribe({
+        next: (res) => this.dencriptedData = res.dencripted_message,
+        error: () => { },
+        complete: () => {
+          const transformed: string[] = []
+          for (let i = 0; i < splitedEnteredData.length; i++) {
+            for (let j = 0; j < this.alphabet.length; j++) {
+              this.dencriptedData[i] == this.alphabet[j].value
+                ? transformed.push(this.alphabet[j].letter)
+                : 0;
+            }
+          }
+          this.dencriptedDataString = transformed;
+          this.saveEncriptedData({
+            fileName: 'decriptedMessage',
+            text: this.dencriptedDataString?.toString(),
+          });
+        }
       });
-    this.saveEncriptedData({
-      fileName: 'decriptedMessage',
-      text: this.dencriptedData?.toString(),
-    });
+    
   }
 
   private setting = {
